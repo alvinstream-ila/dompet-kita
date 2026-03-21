2d3c876c94ae8db6cd6bfceff490fd7968643149
Muhammad Alvin Alhazra <alvinnostream@gmail.com>
2026-03-21 18:51:46 +0700

    optimize SMTP config for Gmail connectivity

--- backend/config/mail.php
+++ backend/config/mail.php
@@ -46,8 +46,8 @@ return [
             'username' => env('MAIL_USERNAME'),
             'password' => env('MAIL_PASSWORD'),
             'encryption' => env('MAIL_ENCRYPTION'),
-            'timeout' => 60,
-            'local_domain' => env('MAIL_EHLO_DOMAIN', parse_url((string) env('APP_URL', 'http://localhost'), PHP_URL_HOST)),
+            'timeout' => 30,
+            'local_domain' => env('MAIL_EHLO_DOMAIN'),
         ],
 
         'ses' => [
