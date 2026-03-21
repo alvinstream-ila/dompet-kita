afd9a4f3ffa95659b7b1778b149755ea20d1bc98
Muhammad Alvin Alhazra <alvinnostream@gmail.com>
2026-03-21 09:37:40 +0700

    chore: bust nixpacks cache with composer.json modify

---
+++ automation/railway/query-repo.js
@@ -0,0 +1,49 @@
+const https = require('https');
+
+const API_KEY = 'a9c4a92c-64da-415b-b334-a98b95550826';
+const SERVICE_ID = '9415fce4-8d0b-4c75-bd92-024e5d78caa0';
+
+async function queryRailway(query, variables = {}) {
+  const data = JSON.stringify({ query, variables });
+  const options = {
+    hostname: 'backboard.railway.app',
+    path: '/graphql/v2',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${API_KEY}`,
+      'Content-Length': Buffer.byteLength(data)
+    }
+  };
+
+  return new Promise((resolve, reject) => {
+    const req = https.request(options, (res) => {
+      let body = '';
+      res.on('data', (d) => body += d);
+      res.on('end', () => resolve(JSON.parse(body)));
+    });
+    req.on('error', reject);
+    req.write(data);
+    req.end();
+  });
+}
+
+const serviceQuery = `
+query service($id: String!) {
+  service(id: $id) {
+    id
+    name
+    repoTriggers {
+      edges {
+        node {
+          branch
+        }
+      }
+    }
+  }
+}
+`;
+
+queryRailway(serviceQuery, { id: SERVICE_ID }).then(res => {
+    console.log(JSON.stringify(res, null, 2));
+}).catch(console.error);
