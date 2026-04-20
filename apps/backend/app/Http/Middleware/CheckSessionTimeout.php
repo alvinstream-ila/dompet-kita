1a18ae1f33534237b5a43c3721f05a0ff585c446
Muhammad Alvin Alhazra <alvinnostream@gmail.com>
2026-04-20 16:00:43 +0700

    style(session): fix unary operator spaces in middleware

--- apps/backend/app/Http/Middleware/CheckSessionTimeout.php
+++ apps/backend/app/Http/Middleware/CheckSessionTimeout.php
@@ -5,9 +5,9 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Auth\AuthenticationException;
 use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
 use Laravel\Sanctum\PersonalAccessToken;
 use Symfony\Component\HttpFoundation\Response;
-use Illuminate\Support\Carbon;
 
 class CheckSessionTimeout
 {
