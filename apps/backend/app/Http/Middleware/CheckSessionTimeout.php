376625ae4c1538edb7f6fdc053ad0c8950076cef
Muhammad Alvin Alhazra <alvinnostream@gmail.com>
2026-04-20 16:06:32 +0700

    style(session): final import ordering and styling cleanup

--- apps/backend/app/Http/Middleware/CheckSessionTimeout.php
+++ apps/backend/app/Http/Middleware/CheckSessionTimeout.php
@@ -5,9 +5,9 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Auth\AuthenticationException;
 use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
 use Laravel\Sanctum\PersonalAccessToken;
 use Symfony\Component\HttpFoundation\Response;
-use Illuminate\Support\Carbon;
 
 class CheckSessionTimeout
 {
@@ -28,7 +28,7 @@ class CheckSessionTimeout
             /** @var PersonalAccessToken|null $token */
             $token = $user->currentAccessToken();
 
-            if (!($token instanceof PersonalAccessToken)) {
+            if (!$token instanceof PersonalAccessToken) {
                 return $next($request);
             }
 
