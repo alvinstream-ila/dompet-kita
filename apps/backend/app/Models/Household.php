70a48c04db0932a417a3155f22fc4e3f06a18abb
Muhammad Alvin Alhazra <alvinnostream@gmail.com>
2026-04-21 23:23:35 +0700

    fix(arch): remove unused factory trait from Household model

--- apps/backend/app/Models/Household.php
+++ apps/backend/app/Models/Household.php
@@ -2,9 +2,7 @@
 
 namespace App\Models;
 
-use Database\Factories\HouseholdFactory;
 use Illuminate\Database\Eloquent\Collection;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
@@ -17,8 +15,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
  */
 class Household extends Model
 {
-    /** @use HasFactory<HouseholdFactory> */
-    use HasFactory, SoftDeletes;
+    use SoftDeletes;
 
     /**
      * The attributes that are mass assignable.
